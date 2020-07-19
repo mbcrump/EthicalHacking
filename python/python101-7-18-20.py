de22d0bcb62d9cea76fb43b39caecab69b070b28
Michael Crump <mbcrump29@gmail.com>
2020-07-19 11:36:55 -0700

    Create python101-7-18-20.py

---
+++ python/python101-7-18-20.py
@@ -0,0 +1,61 @@
+import pyperclip
+from math import *
+
+print ("hello world!")
+print ('hello')
+
+print ('What is your name?')
+your_name = 'Michael'
+#your_name = input()
+your_age = 41
+
+print ('Your name is ' + your_name)
+print ('Your age is ' + str(your_age))
+
+print('This is a \nnew line \nfor all to see')
+
+print('\"I love Azure\" so much')
+print ('hello\john')
+
+phase = "Hello, Twitch world"
+print (phase)
+
+print (phase + ' you are aw4some')
+
+print (phase.lower())
+print (phase.upper())
+##print (phase.lower())
+
+print (len(phase))
+
+print('|' + phase[6] + '|' )
+
+print (phase.lower().index('t'))
+
+print (phase.replace('world', 'universe'))
+
+## end of strings
+
+## using external libraries 
+
+pyperclip.copy("Hello, from mbcrump's Twitch stream!")
+
+## end of ext libraries
+
+foo3 = 4+4
+foo = 10%3
+
+print(foo)
+
+foo1 = -5
+print(abs(foo1))
+
+print(pow(3,3))
+
+print (min(34, 221, 6, 88))
+
+print(round(32.6))
+
+print(sqrt(31))
+
+### end of math
